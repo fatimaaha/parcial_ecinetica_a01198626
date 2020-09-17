0e6057db15577354b7e2a33f1d03aa64da2c4917
fatimaaha <70342536+fatimaaha@users.noreply.github.com>
2020-09-17 13:28:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Ec =")
+    energia_cinetica = velocidad ** 2 * masa / 2
+    basic.showNumber(energia_cinetica)
+})
+let energia_cinetica = 0
+let velocidad = 0
+let masa = 0
+basic.showString("Fatima Herrera A01198626")
+masa = 17.78
+velocidad = 15
